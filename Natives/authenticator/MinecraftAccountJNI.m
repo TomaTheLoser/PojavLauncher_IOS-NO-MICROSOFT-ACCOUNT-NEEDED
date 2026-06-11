74cb377648ebe38d1f077010b3c308ecd9368ff9
TomaTheLoser <gtkgaming6236@gmail.com>
2026-06-11 23:39:39 +0700

    Update MinecraftAccountJNI.m

--- Natives/authenticator/MinecraftAccountJNI.m
+++ Natives/authenticator/MinecraftAccountJNI.m
@@ -2,15 +2,15 @@
 #include "jni.h"
 
 JNIEXPORT jstring JNICALL Java_net_kdt_pojavlaunch_value_MinecraftAccount_getAccessTokenFromKeychain(JNIEnv *env, jclass clazz, jstring xuid) {
-    // This function should only be called once
-    static BOOL called = NO;
-    if (called) {
-        abort();
-    }
-    called = YES;
-
     const char *xuidC = (*env)->GetStringUTFChars(env, xuid, 0);
-    NSString *accessToken = [NSClassFromString(@"MicrosoftAuthenticator") tokenDataOfProfile:@(xuidC)][@"accessToken"];
+    NSString *xuidStr = @(xuidC);
     (*env)->ReleaseStringUTFChars(env, xuid, xuidC);
-    return (*env)->NewStringUTF(env, accessToken.UTF8String);
+
+    // Local accounts use a zeroed XUID — return a dummy token so the game starts offline
+    if ([xuidStr isEqualToString:@"00000000-0000-0000-0000-000000000000"] || xuidStr.length == 0) {
+        return (*env)->NewStringUTF(env, "0");
+    }
+
+    NSString *accessToken = [NSClassFromString(@"MicrosoftAuthenticator") tokenDataOfProfile:xuidStr][@"accessToken"];
+    return (*env)->NewStringUTF(env, accessToken ? accessToken.UTF8String : "");
 }
